1b5556493ccf1b0a5b5b0a60651e43c2ca7d2efa
black141312 <aditya141312@gmail.com>
2026-01-02 23:03:21 +0530

    Initial storefront generation by EpicMerch

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { AnimatePresence, motion } from 'framer-motion'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import ProductDetail from './pages/ProductDetail'
+import Cart from './pages/Cart'
+import Checkout from './pages/Checkout'
+
+function App() {
+  const location = useLocation()
+
+  return (
+    <div className="min-h-screen flex flex-col bg-white text-gray-900">
+      <Navbar />
+      <main className="flex-1">
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </AnimatePresence>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
